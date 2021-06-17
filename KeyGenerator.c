03f6a57e8b61c97900fc45913a32327e9c949e34
Naveen Kr <72698014+naveenkr123@users.noreply.github.com>
2021-06-17 20:16:47 +0530

    Add files via upload

---
+++ KeyGenerator.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>  //#include<windows.h    for windows
+#include<time.h>
+
+void key(){
+    char num[10]={'0','1','2','3','4','5','6','7','8','9'};
+    char alphabet[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+    char temp;
+    srand(time(0));
+    for(int i=1;i<20;i++){
+        if(i==5 || i==10 || i==15)
+        temp='-';
+        else if(i==3 || i==6 || i==12 || i==19)
+        temp=num[rand()%10];
+        else
+        temp=alphabet[rand()%26];
+    printf("%c",temp);
+    }
+}
+
+int main(){
+    for(int i=0;i<5;i++){
+        key();
+        printf("\n");
+        sleep(1);
+        //Sleep(1000) for windows
+    }
+    return 0;
+}
