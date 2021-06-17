7bd20382502069a995f706c0c8c1c7e6e41d2fbc
Naveen Kr <72698014+naveenkr123@users.noreply.github.com>
2021-06-17 20:24:05 +0530

    Update KeyGenerator.c

--- KeyGenerator.c
+++ KeyGenerator.c
@@ -1,8 +1,9 @@
+//This program can generate random serial keys, you can use those keys as a strong password.
+//This program is made for linux. For windows, you need to do changes at lines 5 and 26.
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>  //#include<windows.h    for windows
 #include<time.h>
-
 void key(){
     char num[10]={'0','1','2','3','4','5','6','7','8','9'};
     char alphabet[26]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
@@ -18,7 +19,6 @@ void key(){
     printf("%c",temp);
     }
 }
-
 int main(){
     for(int i=0;i<5;i++){
         key();
